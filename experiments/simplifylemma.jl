787f699d92f488cba3dfac9037a5f07afb35d92d
TaoChenImperial <t.chen24@imperial.ac.uk>
2025-08-26 16:01:15 +0100

    lemma verification

--- experiments/simplifylemma.jl
+++ experiments/simplifylemma.jl
@@ -1,49 +1,40 @@
 using LinearAlgebra, Test
 
-
-n = 10; u,v,d,w,s = [randn(n) for _=1:5]
-A = tril(u*v',-1) + Diagonal(d) + triu(w*s',1)
-uʲ = [0; u[2:end]]
-e₁ = [1; zeros(n-1)]
-B = A + uʲ * uʲ'A + uʲ * e₁'A
-@test count(≥(1E-13), svdvals((B)[5:end,1:4])) == 1
-@test count(≥(1E-13), svdvals((B)[1:4,5:end])) == 2
-
-
-u¹ = u[2:end]; v¹ = v[2:end]; d¹ = d[2:end]; w¹ = w[2:end]; s¹ = s[2:end]; k,q = randn(2)
-B =  tril(u¹*v¹',-1) + Diagonal(d¹) + triu(w¹*s¹',1)
-C = B + k * u¹*s¹' + q * u¹ * u¹'B
-y = C[:,1]
-y[1] += norm(y); y = y/norm(y)
-Q = I-2y*y'
-@test count(≥(1E-13), svdvals((Q*C)[5:end,1:4])) == 1
-@test count(≥(1E-13), svdvals((Q*C)[1:4,5:end])) == 2
-
-
-
-
-
-function diag_plus_semi_plus_specialrank1(u,v, d, w, s, k, q)
+function diag_plus_semi_plus_specialrank1(u,v, d, w, s, q, k)
     B =  tril(u*v',-1) + Diagonal(d) + triu(w*s',1) # diag+Semi
-    B + u * (k*s' + q * u'B) # + special rank 1 == (I+ q * u*u')*B + k*u*s'
+    B + u * (q*s' + k * u'B) # + special rank 1 == (I+ q * u*u')*B + k*u*s'
 end
 
-n = 10; u,v,d,w,s = [randn(n) for _=1:5]; k,q = randn(2);
-A = diag_plus_semi_plus_specialrank1(u,v, d, w, s, k, q)
+n = 10; u,v,d,w,s = [randn(n) for _=1:5]; q,k = randn(2);
+B = tril(u*v',-1) + Diagonal(d) + triu(w*s',1)
+A = diag_plus_semi_plus_specialrank1(u,v, d, w, s, q, k)
 @test count(≥(1E-13), svdvals((A)[5:end,1:4])) == 1
 @test count(≥(1E-13), svdvals((A)[1:4,5:end])) == 2
 
-y = A[:,1]; y[1] += norm(y); y = y/norm(y); Q = I-2y*y'
+y = A[:,1]; y[1] += sign(y[1])*norm(y); y = y/norm(y); Q = I-2y*y'
 @test count(≥(1E-13), svdvals((Q*A)[5:end,1:4])) == 1
 @test count(≥(1E-13), svdvals((Q*A)[1:4,5:end])) == 2
 
 # just submatrix
-ũ,ṽ,d̃,w̃,s̃, k̃,q̃ = u[2:end],v[2:end],d[2:end],w[2:end],s[2:end],k,q # ????
-@test_broken (Q*A)[2:end,2:end] == diag_plus_semi_plus_specialrank1(ũ,ṽ,d̃,w̃,s̃, k̃,q̃)
+u_2 = u[1:end]
+u_2[1] = 0
+τ = 2*y[1]*y[1]
+k̅ = sqrt(2*y[2]^2/(τ*u[2]^2))*(sign(y[1])*sign(y[2])/sign(u[2]))
+γ = -τ*q*e₁'*u-τ*q*k̅*u_2'*u
+ϕ = -τ*q*k̅*e₁'*u-τ*q*k̅^2*u_2'*u
+ξ = -τ*k*e₁'*u-τ*k*k̅*u_2'*u
+η = -τ*k*k̅*e₁'*u-τ*k*k̅^2*u_2'*u
 
-# full ,matrix
-ũ,ṽ,d̃,w̃,s̃, k̃,q̃ = u,v,d,w,s,k,q # ????
-@test_broken (Q*A) == diag_plus_semi_plus_specialrank1(ũ,ṽ,d̃,w̃,s̃, k̃,q̃)
+ũ,ṽ,d̃,w̃,s̃= u[2:end],v[2:end],d[2:end],w[2:end],s[2:end]
+q̃ = q + k*u[1]*w[1]-k̅*τ*w[1]+ϕ+η*u[1]*w[1]
+k̃ = k - k̅*k̅*τ + η
+(I-τ*(e₁+k̅ *u_2)*(e₁'+k̅ *u_2'))*A
+@test (Q*A)[2:end,2:end] ≈ diag_plus_semi_plus_specialrank1(ũ,ṽ,d̃,w̃,s̃,q̃,k̃)
+
+α = w[1] + q*u[1] + k*u[1]^2*w[1] - τ*w[1] + γ - k*u[1]^2*w[1] + τ*k̅*w[1]*u[1]
+β = k*u[1] - τ*k̅ + ξ
+ss = u'B
+@test (Q*A)[1,2:end]' ≈ α*s[2:end]' + β*ss[2:end]'
 
 # actually, want to show that the factors matrix has this structure....
 F = LinearAlgebra.qrfactUnblocked!(copy(A)).factors
